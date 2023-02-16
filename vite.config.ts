6534eb0f7d2facf1d14fb7e36511aa356aa76d7c
mirai-mirai <shiozaki.takashi@gmail.com>
2023-02-16 09:29:01 +0900

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/20230204_ImageTrimmer/', // github pagesの変な仕様（ルートが１つ上）に対応する
+  // assetsInclude: ["**/*.bin", "**/*.json"],// うまくモデルを読み込めない問題
   plugins: [vue()],
+  build: {
+    outDir: 'docs', // ビルド出力先を変更
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name]-[hash][extname]',
+        chunkFileNames: '[name]-[hash].js', // JSをルートに出力
+        entryFileNames: '[name]-[hash].js', // JSをルートに出力
+      },
+    },
+  },
 })
